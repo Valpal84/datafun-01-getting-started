07efbd98a4067251ab565e71ece48f5c7461d63d
Valerie Johnson <valeriecrist@gmail.com>
2023-08-23 19:06:45 -0500

    TODO 2 basic expressions added rectangle

--- basic_expressions.py
+++ basic_expressions.py
@@ -40,16 +40,19 @@ logger, logname = setup_logger(__file__)
 # TODO: Try changing the values of these variables
 # TODO: Add some new variables (like rectangle_length and rectangle_width)
 #       and calculate the area of a rectangle (rectangle_area = rectangle_length * rectangle_width)
-triangle_base = 10
-triangle_height = 5
-num1 = 50
-num2 = 20
+triangle_base = 20
+triangle_height = 8
+rectangle_length = 15
+rectangle_width = 25
+num1 = 40
+num2 = 15
 float_num1 = 1.1
 float_num2 = 2.2
 float_num3 = 3.3
 
 # Basic Arithmetic Operations
 triangle_area = triangle_base * triangle_height / 2
+rectangle_area = rectangle_length * rectangle_width
 total_sum = float_num1 + float_num2
 difference = num1 - num2
 
@@ -61,3 +64,5 @@ logger.info(
     f"Given float_num1={float_num1} and float_num2={float_num2}, sum = {total_sum}"
 )
 logger.info(f"Given num1={num1} and num2={num2}, the difference = {difference}")
+logger.info(f"Given length={rectangle_length} and width={rectangle_width}, rectangle area = {rectangle_area}")
+
